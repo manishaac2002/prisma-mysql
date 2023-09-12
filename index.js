93314d79585951be1b2127e5bb8c4caa27c43240
manishaac2002 <manishaac2002@gmail.com>
2023-09-12 16:03:03 +0530

    chore--

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 const application = express()
-const PORT =3000
+const PORT = 3000
 
 application.use(express.json())
 
-application.listen(PORT,()=>{
+application.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
